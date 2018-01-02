c0e32752638facde0f4289cf5f364cbf9dbab7bf
wackdot <e0167324@u.nus.edu>
2018-01-02 16:02:57 +0800

    2/1/18 4:02 PM

--- src/shorterner/models.py
+++ src/shorterner/models.py
@@ -3,12 +3,61 @@ from django.db import models
 class Urls(models.Model):
     short_url = models.CharField(max_length=200)
     input_url = models.CharField(max_length=200)
+    analytics = models.OneToOneField(Analytics, on_delete=models.CASCADE, primary_key=True)),
 
     @classmethod
     def create(cls, short_url, input_url):
         urls = cls(
             short_url=short_url, 
-            input_url=input_url
+            input_url=input_url,
             )
         return urls
 
+    def __str__(self):
+        return self.short_url
+
+class Analytics(model.Model):
+    status = models.CharField(max_length=10)
+    created = models.DateTimeField()
+    short_url_clicks = models.IntegerField()
+    long_url_clicks = models.IntegerField()
+ 
+class Referrers(model.Model):
+    analytics_detail = models.ForeignKey(Analytics, on_delete=models.CASCADE)
+    count = models.IntegerField()
+    id = models.CharField(max_length=200)
+
+class Countries(model.Model):
+    analytics_detail = models.ForeignKey(Analytics, on_delete=models.CASCADE)
+    count = models.IntegerField()
+    id = models.CharField(max_length=200)
+
+class Browsers(model.Model):
+    analytics_detail = models.ForeignKey(Analytics, on_delete=models.CASCADE)
+    count = models.IntegerField()
+    id = models.CharField(max_length=200)
+
+class Platforms(model.Model):
+    analytics_detail = models.ForeignKey(Analytics, on_delete=models.CASCADE)
+    count = models.IntegerField()
+    id = models.CharField(max_length=200)
+
+class Month(model.Model):
+    analytics_detail = models.ForeignKey(Analytics, on_delete=models.CASCADE)
+    count = models.IntegerField()
+    id = models.CharField(max_length=200)
+
+class Week(model.Model):
+    analytics_detail = models.ForeignKey(Analytics, on_delete=models.CASCADE)
+    count = models.IntegerField()
+    id = models.CharField(max_length=200)
+
+class Day(model.Model):
+    analytics_detail = models.ForeignKey(Analytics, on_delete=models.CASCADE)
+    count = models.IntegerField()
+    id = models.CharField(max_length=200)
+
+class TwoHours(model.Model):
+    analytics_detail = models.ForeignKey(Analytics, on_delete=models.CASCADE)
+    count = models.IntegerField()
+    id = models.CharField(max_length=200)
--- src/shorterner/views.py
+++ src/shorterner/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.conf import settings
 from django.urls import reverse
@@ -10,25 +10,7 @@ import json
 
 from .forms import SubmitUrlForm
 
-GOOGLE_URL_SHORTEN_API = 'AIzaSyC5OU6r75zEYM6EhewVg3rcBqOck7ZVBJg'
-
-# def shorterner_index(request):
-#     if request.method == "POST":
-#         form = SubmitUrlForm(request.POST)
-#         if form.is_valid():
-#             input_url = form.cleaned_data['url']
-#             short_url = google_url_shorten(input_url)
-
-#             print(input_url)
-#             print(short_url)
-
-#             new_url = Urls.create(short_url, input_url)
-#             new_url.save()
-
-#             return HttpResponseRedirect('shorterner/list.html')
-#     else:
-#         form = SubmitUrl()
-#     return render(request, 'shorterner/request.html', {'form': form})
+API_KEY = 'AIzaSyC5OU6r75zEYM6EhewVg3rcBqOck7ZVBJg'
 
 class RequestView(View):
     form_class = SubmitUrlForm
@@ -55,9 +37,8 @@ class RequestView(View):
             new_url = Urls.create(short_url, input_url)
             new_url.save()
 
-            return render(request, '/shorterner/request/', {'id': ''})
+            return HttpResponseRedirect(reverse('shorterner:list'))
         return render(request, self.template_name, {'form': form})
-
 
 class IndexView(generic.ListView):
     template_name = 'shorterner/list.html'
@@ -66,12 +47,22 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Urls.objects.all()
 
+# class DetailView(generic.DetailView):
+
 
 
 def google_url_shorten(url):
-   req_url = 'https://www.googleapis.com/urlshortener/v1/url?key=' + GOOGLE_URL_SHORTEN_API
+   req_url = 'https://www.googleapis.com/urlshortener/v1/url?key=' + API_KEY
    payload = {'longUrl': url}
    headers = {'content-type': 'application/json'}
    r = requests.post(req_url, data=json.dumps(payload), headers=headers)
    resp = json.loads(r.text)
    return resp['id']
+
+def google_url_expand(self, url):
+    req_url = 'https://www.googleapis.com/urlshortener/v1/url'
+    payload = {'key': self.API_KEY, 'shortUrl': url}
+    r = requests.get(req_url, params=payload)
+    resp = json.loads(r.text)
+    return resp['longUrl']
+
